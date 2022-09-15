421f477a4a7d94fc57f3f261e86079738a756320
macarenasv <macarensotovaldes@@gmail.com>
2022-09-15 10:02:55 -0300

    Âcambios hasta 15sept

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 
 
 let form = document.getElementById("form");
-let input = document.getElementById("input");
-let descripciones = document.getElementById("descripciones");
 
+let descripciones = document.getElementById("descripciones");
+let lista = [];
 let parque = document.getElementById("nombreParque");
 let fecha = document.getElementById("fecha");
 let planes = document.getElementById("planes");
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    console.log("button")
+    
     botonEnviar();
 })
 
 
- var agrega = getElementById("agregar");
-
- agrega.addEventListener(click, guardarDescripcion)
 
 
 
 let botonEnviar = () =>{
-    if(input.value === ""){
+    if(parque.value === ""|| fecha.value === ""||planes.value === ""){
         alert("Los campos no pueden quedar en blanco.")
     }
     else{
-        console.log("enviado")
         guardarDescripcion()
     }
 };
@@ -38,32 +34,40 @@ let guardarDescripcion = ()=> {
         parque: parque.value,
         fecha: fecha.value,
         planes: planes.value, 
+        id: lista.length?lista[lista.length -1].id+1:1,
     }
-    console.log(parque.value);
-
-    };
-    
-    // crearDescripcion();
-    
-
-
-let crearDescripcion = () => {
-    descripciones.innerHTML += 
-    `
-    <div>
-    <p>${descripcion.texto}</p>
-    <span class="opciones">
-        <i onClick ="editarDescripcion(this)" class="fa-solid fa-pen"></i>
-        <i onClick ="borrarDescripcion(this)" class="fa-solid fa-trash-can"></i>
-    </span>
-</div>
-    `;
-    input.value = "";
+    console.log(descripcion);
+    lista.push(descripcion);
+    //local storage
+    form.reset(); 
+    crearDescripcion();
 };
 
 
-let borrarDescripcion = (e)=> {
-e.parentElement.parentElement.remove();
+let crearDescripcion = () => {
+    descripciones.innerHTML = ""
+    lista.forEach((des)=>{
+        descripciones.innerHTML += 
+        `
+        <div>
+        <p>${des.parque}</p>
+        <p>${des.fecha}</p>
+        <p>${des.planes}</p>
+        <span class="opciones">
+            <i onClick="editarDescripcion(this)" class="fa-solid fa-pen"></i>
+            <i onClick="borrarDescripcion(this,${des.id})" class="fa-solid fa-trash-can"></i>
+        </span>
+    </div>
+        `;
+    })
+    
+    
+};
+
+let borrarDescripcion = (e,idDelElementoBorrado)=> {
+lista = lista.filter((b)=> b.id!=idDelElementoBorrado)
+//local storage
+crearDescripcion()
 };
 
 let editarDescripcion = (e)=> {
